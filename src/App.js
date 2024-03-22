1af0e73734ad1e528d02b2fbd8a7cb8460cd8c03
zishan07cse <zishan07csecuet@gmail.com>
2024-03-23 00:03:50 +0600

    Fix:date format is changed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const currentDate = new Date();
+  const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+  const formattedDate = currentDate.toLocaleDateString('en-GB', options); 
   const currDate = new Date().toLocaleDateString();
   const currTime = new Date().toLocaleTimeString();
 
@@ -10,7 +13,7 @@ function App() {
       <div id="clockdate">
           <div class="clockdate-wrapper">
             <h1 class="city">This is in Dhaka City </h1>
-            <div id="clock"><h1>Current Date is {currDate}</h1></div>
+            <div id="clock"><h1>Current Date is {formattedDate}</h1></div>
             <div id="date"><h1>Current Time is {currTime}</h1></div>
           </div>
         </div>
